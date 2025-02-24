e2b8c58ad7567ffe19947d8f736b8efffd877864
hoondori <hoondori@gmail.com>
2025-02-24 18:24:34 +0900

    commit

--- bmi_animator/lib/main.dart
+++ bmi_animator/lib/main.dart
@@ -34,10 +34,11 @@ class _MyHomePageState extends State<MyHomePage> {
   List<People> _people = new List.empty(growable: true);
   int _current = 0;
   Color _weightColor = Colors.blue;  // 몸무게 레벨에 따라 몸무게 막대 그래프 색상 변경
+  double _opacity = 1.0;
 
   @override
   void initState() {
-    _people.add(People('Smith', 100, 92));
+    _people.add(People('Smith', 120, 92));
     _people.add(People('Mary', 194, 140));
     _people.add(People('Hoondori', 165, 73));
     super.initState();
@@ -55,47 +56,51 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Center(
           child: Column(
             children: [
-              SizedBox(
-                height: 200,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    SizedBox(width: 100, child: Text('이름: ${_people[_current].name}')),
-                    AnimatedContainer( // 키에 비례해서 막대 그래프
-                      width: 50,
-                      height: _people[_current].height,
-                      duration: Duration(seconds: 2),
-                      curve: Curves.bounceIn,
-                      color: Colors.amber,
-                      child: Text(
-                        '키: ${_people[_current].height}',
-                        textAlign: TextAlign.center,
+              AnimatedOpacity(
+                opacity: _opacity,
+                duration: Duration(seconds: 1),
+                child: SizedBox(
+                  height: 200,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      SizedBox(width: 100, child: Text('이름: ${_people[_current].name}')),
+                      AnimatedContainer( // 키에 비례해서 막대 그래프
+                        width: 50,
+                        height: _people[_current].height,
+                        duration: Duration(seconds: 2),
+                        curve: Curves.bounceIn,
+                        color: Colors.amber,
+                        child: Text(
+                          '키: ${_people[_current].height}',
+                          textAlign: TextAlign.center,
+                        ),
                       ),
-                    ),
-                    AnimatedContainer( // 몸무게에 비례해서 막대 그래프
-                      width: 50,
-                      height: _people[_current].weight,
-                      duration: Duration(seconds: 2),
-                      curve: Curves.easeInCubic,
-                      color: _weightColor,
-                      child: Text(
-                        '몸무게: ${_people[_current].weight}',
-                        textAlign: TextAlign.center,
+                      AnimatedContainer( // 몸무게에 비례해서 막대 그래프
+                        width: 50,
+                        height: _people[_current].weight,
+                        duration: Duration(seconds: 2),
+                        curve: Curves.easeInCubic,
+                        color: _weightColor,
+                        child: Text(
+                          '몸무게: ${_people[_current].weight}',
+                          textAlign: TextAlign.center,
+                        ),
                       ),
-                    ),
-                    AnimatedContainer( // BMI 에 비례해서 막대 그래프
-                      width: 50,
-                      height: _people[_current].bmi,
-                      duration: Duration(seconds: 2),
-                      curve: Curves.linear,
-                      color: Colors.pinkAccent,
-                      child: Text(
-                        'BMI: ${_people[_current].bmi.toString().substring(0, 2)}',
-                        textAlign: TextAlign.center,
+                      AnimatedContainer( // BMI 에 비례해서 막대 그래프
+                        width: 50,
+                        height: _people[_current].bmi,
+                        duration: Duration(seconds: 2),
+                        curve: Curves.linear,
+                        color: Colors.pinkAccent,
+                        child: Text(
+                          'BMI: ${_people[_current].bmi.toString().substring(0, 2)}',
+                          textAlign: TextAlign.center,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
               ElevatedButton(
@@ -119,6 +124,14 @@ class _MyHomePageState extends State<MyHomePage> {
                     }
                   });
                 }
+              ),
+              ElevatedButton(
+                  child: Text("사라지기"),
+                  onPressed: (){
+                    setState(() {
+                      _opacity == 1 ? _opacity = 0.3: _opacity = 1;
+                    });
+                  }
               ),
             ]
           )
