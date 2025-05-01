2c3fe2614ce9e973c96410609fce50a99414e417
hoondori <hoondori@gmail.com>
2025-05-02 05:54:53 +0900

    하단 Navigator와 sync된 상단 TabView 구현

--- bamtol_market_app/lib/common/controller/bottom_nav_controller.dart
+++ bamtol_market_app/lib/common/controller/bottom_nav_controller.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class BottomNavController extends GetxController {
+class BottomNavController extends GetxController with GetTickerProviderStateMixin {
 
   RxInt menuIndex = 0.obs;
+  late TabController tabController;
+
+  @override
+  void onInit() {
+    super.onInit();
+    tabController = TabController(length: 5, vsync: this);
+  }
 
   void changeBottomNav(int index) {
     menuIndex(index);
+    tabController.animateTo(index);
   }
 }
